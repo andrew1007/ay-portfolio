12e467be7c8d21f1ef0417974d496416870a7fc2
andrew1007 <yueh.andrew@gmail.com>
2017-08-07 10:01:52 -0700

    DRYed up some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
-import IntroContainer from './components/intro/intro'
-import SkillsContainer from './components/containers/skills'
-import ProjectsContainer from './components/containers/projects'
+import IntroContainer from './components/containers/intro_container'
+import SkillsContainer from './components/containers/skills_container'
+import ProjectsContainer from './components/containers/projects_container'
 
 class App extends Component {
-  // <Intro/>
   render() {
     return (
       <div className="App">
