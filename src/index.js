e4cde1b6e6624cb17b9045b6f04ce91c63b9572a
andrew1007 <yueh.andrew@gmail.com>
2017-08-16 15:36:55 -0700

    added periodic pinging

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import http from "http";
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-require('newrelic');
 
 document.addEventListener('DOMContentLoaded', () => {
+  setInterval(function() {
+    http.get("https://ay-portfolio.herokuapp.com/");
+  }, 300000);
+
   ReactDOM.render(<App/>, document.getElementById('root'));
   registerServiceWorker();
 })
