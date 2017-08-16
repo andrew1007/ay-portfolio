4b988dc029ef418391dcef19e88f9fddf7a34a88
andrew1007 <yueh.andrew@gmail.com>
2017-08-16 14:54:18 -0700

    added newrelic

--- src/components/about_me/about_me.js
+++ src/components/about_me/about_me.js
@@ -3,10 +3,11 @@ import React from 'react'
 const AboutMe = () => (
   <div className="about-me-container">
     <p className="about-me-text">
-      I'm a chemical engineer, turned web developer.
-      I found my passion in graduate school, which was my
+      I'm a software engineer.
+      I found my passion in graduate school, where I hold a
+      Master's Degree in Chemical Engineering. It was my
       first exposure to basic programming. I liked it so much,
-      I switched careers. I love what I do, and I hope to
+      I switched careers. I love what I do and I hope to
       share my abilities as a Software Engineer.
     </p>
   </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+require('newrelic');
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App/>, document.getElementById('root'));
