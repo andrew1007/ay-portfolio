054428670608705dae90958cb4b36485c1ebc102
andrew1007 <yueh.andrew@gmail.com>
2017-08-16 19:28:30 -0700

    newrelic adjustments

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import http from "http";
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import './newrelic.js'
 import Favicon from 'react-favicon'
+require('./newrelic')
 
 document.addEventListener('DOMContentLoaded', () => {
-  setInterval(function() {
-    http.get("https://ay-portfolio.herokuapp.com/");
-  }, 300000);
-
   <Favicon url={[null]}/>
-
   ReactDOM.render(<App/>, document.getElementById('root'));
   registerServiceWorker();
 })
