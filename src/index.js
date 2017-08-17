52f840038a57827120d6b2d251d81ad261e62c49
andrew1007 <yueh.andrew@gmail.com>
2017-08-16 19:12:52 -0700

    removed favicon

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 import http from "http";
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './newrelic.js'
+import Favicon from 'react-favicon'
 
 document.addEventListener('DOMContentLoaded', () => {
   setInterval(function() {
     http.get("https://ay-portfolio.herokuapp.com/");
   }, 300000);
+
+  <Favicon url={[null]}/>
 
   ReactDOM.render(<App/>, document.getElementById('root'));
   registerServiceWorker();
