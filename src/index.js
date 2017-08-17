141e3a7e1b7ce3fa1c3bf9cd53f922b8ff15d2d3
andrew1007 <yueh.andrew@gmail.com>
2017-08-16 20:29:43 -0700

    removed favicon

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import Favicon from 'react-favicon'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <div>
       <App/>
-      <Favicon url={[null]}/>
-    </div>
     , document.getElementById('root'));
   registerServiceWorker();
 })
