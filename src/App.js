87eb72fe04f5db3f9ae32dcb1e26f0362a597296
andrew1007 <yueh.andrew@gmail.com>
2017-08-19 11:15:00 -0700

    finally fixed scrollbar height bug

--- src/App.js
+++ src/App.js
@@ -15,14 +15,17 @@ containers.set(<ProjectsContainer/>, "projects-container")
 const App = () => {
   const _showId = (id) => {
     $(`#${id}`).removeClass("hidden-true").addClass("hidden-false")
-    $(".navbar-hidden-true").removeClass("navbar-hidden-true")
-    .addClass("navbar-hidden-false")
+    debugger
+    if (window.pageYOffset > 0) {
+      $(".navbar-hidden-true").removeClass("navbar-hidden-true")
+      .addClass("navbar-hidden-false")
+    }
   }
 
   const _hideId = (id) => {
     $(`#${id}`).removeClass("hidden-false").addClass("hidden-true")
-    $(".navbar-hidden-true").removeClass("navbar-hidden-true")
-    .addClass("navbar-hidden-false")
+      $(".navbar-hidden-true").removeClass("navbar-hidden-true")
+      .addClass("navbar-hidden-false")
   }
 
   const _showNavBackground = () => {
@@ -40,13 +43,13 @@ const App = () => {
   }
 
   const renderContainers = Array.from(containers).map((section, idx) => {
-    if (section[1] === "intro-container") {
-      return (
-        <div key={idx} id={section[1]} className="hidden-false">
-          {section[0]}
-        </div>
-      )
-    } else {
+    // if (section[1] === "intro-container") {
+    //   return (
+    //     <div key={idx} id={section[1]} className="hidden-false">
+    //       {section[0]}
+    //     </div>
+    //   )
+    // } else {
       return (
         <Waypoint
           onEnter={() => _showId(section[1])}
@@ -58,7 +61,7 @@ const App = () => {
             </div>
           </Waypoint>
         )
-    }
+    // }
   })
 
   return (
@@ -66,10 +69,8 @@ const App = () => {
       <Waypoint
         onLeave={() => _showNavBackground()}
         onEnter={() => _hideNavBackground()}
-        topOffset={"25px"}
-      >
-        <div className="navbar-filler"></div>
-      </Waypoint>
+        >
+        </Waypoint>
       <div id="nav-container">
         <NavBar/>
       </div>
