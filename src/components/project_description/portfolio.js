215d10490a76be67e5164cbe04671b87936c1f99
andrew1007 <yueh.andrew@gmail.com>
2017-09-27 13:18:29 -0700

    changed css margin for first about me component

--- src/components/project_description/portfolio.js
+++ src/components/project_description/portfolio.js
@@ -6,8 +6,8 @@ const Portfolio = () => (
       This Portfolio Website
     </h2>
     <p className="project-description-text">
-      Yep, this is a React.js frontend-only web app. A 100% stateless
-      one as well. There are a lot of templates out there, but I thought
+      Yep, this is a React.js frontend-only web app.
+      There are a lot of templates out there, but I thought
       this would be a great
       opportunity to make another app and get more practice in React.js,
       new ES6 features, and Sass.
