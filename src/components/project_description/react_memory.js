668610ab15e9e5daea5e8ad56536703c4ac66b99
andrew1007 <yueh.andrew@gmail.com>
2018-02-25 09:01:45 -0800

    added react-memory to project list

---
+++ src/components/project_description/react_memory.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ReactMemory = _ => {
+  return (
+    <div>
+      <h2>
+        React Memory
+      </h2>
+      <p className='project-description-text'>
+        A card matching came. Flip over cards and try to match values. Made with React.js (duh) with best practices in mind.
+      </p>
+    </div>
+  )
+}
